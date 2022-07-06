84f8a51f501dd7fb04fe4fd42f4e0bbd9f6d5504
Kate Tyrol <katetyrol@gmail.com>
2022-07-05 23:03:38 -0400

    Fully functioning spell check player

--- SpellCheck.py
+++ SpellCheck.py
@@ -1,30 +1,28 @@
 import Levenshtein
 import PySimpleGUI as psguy
 
-def SpellCheck(target,userInput):
-    return str(Levenshtein.ratio(target,userInput))
-
-# TODO: Add remaining text.
-# TODO: Add a field for a second term.
+# Here is the original contents of the window that the user sees.
 thisLay = [[psguy.Text('Welcome to the SpellCheck Player.\nThis is where you will play around with spellcheck settings.\
     \nA typical spellchecker will tell you if a word is spelled correctly based on some reference word.\
     \nThis SpellCheck player instead tells you how closely any two words or phrases match.\
-    \nA score of 1 means an exact match, and anything less than that is a partial match.\
-    \n\nEnter your reference word ("correctly" spelled) here:')],
-          #[psguy.Input()],
-          [psguy.Input()],
-          [psguy.OK('Check Spelling')]]
+    \nA score of 1 means an exact match, and anything less than that is a partial match.', key = 'lblInfo')],
+        [psguy.Text('Enter your reference word ("correctly" spelled) here:', key = 'lbl1')],
+        [psguy.Input()],
+        [psguy.Text('Enter the word you are checking against that reference here:', key = 'lbl2')],
+        [psguy.Input()],
+        [psguy.OK('Check Spelling')]]
 
 thisWin =psguy.Window('SpellCheck Player', thisLay, margins=(100, 50))
 
+# This loop waits for the user to click "Check Spelling" or close the window, taking the expected action when the user does so.
 while True:
     event, values = thisWin.read()
     if event == 'Check Spelling':
-        #TODO: Get actual values
-        myTarg = 'Jennifer'
-        uIn = 'Yennifer'
-        #TODO: Display these results in the window instead of the terminal
-        print('The score for ' + myTarg + ' and ' + uIn + ' is: ' + SpellCheck(myTarg,uIn))
+        # Parse the input into useful variable names
+        myTarg = values[0]
+        uIn = values[1]
+        # Update just the informational text with the results of the Levenshtein ratio of the two phrases that were input.
+        thisWin['lblInfo'].update('The score for ' + myTarg + ' and ' + uIn + ' is: ' + str(Levenshtein.ratio(myTarg.lower(),uIn.lower())))
     if event == psguy.WIN_CLOSED:
         break
 
