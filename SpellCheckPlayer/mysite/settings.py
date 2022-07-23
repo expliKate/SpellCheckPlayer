0176581c260b76821cc04c15bfe5e295ebacc8e5
Kate Tyrol <katetyrol@gmail.com>
2022-07-23 14:28:46 -0400

    Trying to improve security

--- SpellCheckPlayer/mysite/settings.py
+++ SpellCheckPlayer/mysite/settings.py
@@ -1,3 +1,6 @@
+from telnetlib import SE
+import environ
+
 """
 Django settings for mysite project.
 
@@ -8,9 +11,15 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+
+Modified 7/23/22 by Kate Tyrol
 """
 
 from pathlib import Path
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0*s*sj4s2)v!$k(g#tg!p!$y5tkf#3&ctwc+jp+@rok(n9!3jn'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
